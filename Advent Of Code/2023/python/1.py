ffcde9ac041dfa95377e5b0f74e5bbc7c4ce252f
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2024-02-11 12:30:39 -0500

    Add files via upload

---
+++ Advent Of Code/2023/python/1.py
@@ -0,0 +1,21 @@
+lines = None
+with open('./1.txt', 'r') as file:
+    lines = file.readlines()
+    lines = [line.strip().split() for line in lines]
+
+sum = 0
+
+for i in lines:
+    current = ''
+    for letter in i[0]:
+        if letter.isdigit():
+            current += letter
+
+    if current != '':
+        print(current[0], current[-1])
+        sum += int(str(current[0]) + str(current[-1]))
+    
+
+
+print(sum)
+
