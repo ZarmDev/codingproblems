853a49b9d5c81583665bf63e5416275928e0cdb3
ZarmDev <80705328+ZarmDev@users.noreply.github.com>
2024-02-29 22:00:56 -0500

    Update bovineshuffle.cpp

--- bovineshuffle.cpp
+++ bovineshuffle.cpp
@@ -4,11 +4,28 @@
 #include <vector>
 using namespace std;
 
+void setIO(string s) {
+	ios_base::sync_with_stdio(0);
+	cin.tie(0);
+	freopen((s + ".in").c_str(), "r", stdin);
+	freopen((s + ".out").c_str(), "w", stdout);
+}
+
 int main() {
+	setIO("shuffle");
 	int n; cin >> n;
 	
 	// first artifically create an array wtih n itmes
-	vector<int> originalArray(5);
+	vector<int> originalArray;
+
+	for (int i = 1; i < n + 1; i++) {
+		originalArray.push_back(i);
+	}
+
+	// for (int i = 0; i < (int) originalArray.size(); i++) {
+	// 	cout << originalArray[i] << '\n';
+	// }
+	// cout << "----------" << '\n';
 
 	vector<int> shuffleLocations;
 	// input shuffle
@@ -30,16 +47,20 @@ int main() {
 		// now SHUFFLE!
 		for (int i = 0; i < n; i++) {
 			// go from i -> shuffleLocations[i] and reverse: shuffleLocations[i] (3) -> i (2)
-			// my braincells werent working writing this
-			temporaryNewArr[i] = originalArray[shuffleLocations[i]];
+			// cout << "originalArray[" << shuffleLocations[i] - 1 << "] = " << originalArray[shuffleLocations[i] - 1] << '\n';
+			temporaryNewArr[i] = originalArray[shuffleLocations[i] - 1];
 		}
 		originalArray = temporaryNewArr;
+		// for (int i = 0; i < (int) originalArray.size(); i++) {
+		// 	cout << originalArray[i] << '\n';
+		// }
+		// cout << "------" << '\n';
 	}
 
 	// now compare with the cow ID's
 	for (int i = 0; i < n; i++) {
 		// my braincells werent working writing this
-		cout << originalIDs[originalArray[i]];
-	}
-
+		// cout << "originalIDs[" << originalArray[i] << "] = " << originalIDs[originalArray[i]] << '\n';
+		cout << originalIDs[originalArray[i] - 1] << '\n';
+	}		
 }
